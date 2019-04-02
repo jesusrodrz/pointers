23dab8bad37afeccbb2b1c5b7e2f7f24354475e5
Jesus Rodriguez <jesus.rodrz97@gmail.com>
2019-04-02 19:12:17 -0400

    pointer animation

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 function pointers(element) {
   element.addEventListener('click', e => {
-    console.log(e, e.target)
     const target = e.target
 
     if (target.classList.contains('pointer__icon')) {
       const parent = target.closest('.pointer')
 
       parent.querySelector('.pointer__body').classList.add('active')
+      target.classList.add('unactive')
     }
     if (target.classList.contains('pointer__close')) {
       const parent = target.closest('.pointer')
+      const icon = parent.querySelector('.pointer__icon')
 
       parent.querySelector('.pointer__body').classList.remove('active')
+      icon.classList.remove('unactive')
     }
   })
 }
